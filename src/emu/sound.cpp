e287fe851fdf722f43c4be21a5b5b885ae02346a
gyoungsu <caromdreamer@gmail.com>
2026-07-01 09:12:56 +0900

    Add Kaileron MAME runahead support

--- src/emu/sound.cpp
+++ src/emu/sound.cpp
@@ -45,6 +45,7 @@
 
 
 const attotime sound_manager::STREAMS_UPDATE_ATTOTIME = attotime::from_hz(STREAMS_UPDATE_FREQUENCY);
+bool g_kn_mame_suppress_replay_audio = false;
 
 
 //**// Output buffer management
@@ -997,6 +998,12 @@ void sound_manager::input_get(int id, sound_stream &stream)
 
 void sound_manager::output_push(int id, sound_stream &stream)
 {
+	if (g_kn_mame_suppress_replay_audio)
+	{
+		m_record_samples = 0;
+		return;
+	}
+
 	auto &spk = m_speakers[id];
 	auto &out = spk.m_buffer;
 	auto &inp = stream.m_input_buffer;
